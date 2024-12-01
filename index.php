e588afbcff3d6dcab691827bf0449d9aaafcd9ad
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-12-02 00:29:53 +0500

    icon

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
- 
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 
 </head>
 
